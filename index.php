1531c4724783890f8f877ead240015933c3c603b
João Rodrigues <joaoadriano3@gmail.com>
2018-01-05 13:04:12 +0000

    Fixed code indentation on GitHub and added error checking.

--- index.php
+++ index.php
@@ -11,89 +11,108 @@
   </head>
 
   <body>
-  	<div class="container">
-  		<div class="row">
-  		</div>
+  <div class="container">
+  <div class="row">
+  </div>
 
-  		<div class="row">
-  			<h3 class="center-align white-text"><img src="http://nortegaming.pt/img/logo.png"/> <br>Current Server Status
-  			</h3>
-  		</div>
+  <div class="row">
+  <h3 class="center-align white-text"><img src="http://nortegaming.pt/img/logo.png"/> <br>Current Server Status</h3>
+  </div>
 
-  		<div class="row">
-  <?php
-	require 'SourceQuery/bootstrap.php';
+  <div class="row">
+    <?php
+    require 'SourceQuery/bootstrap.php';
 
-	use xPaw\SourceQuery\SourceQuery;
-	Header('X-Content-Type-Options: nosniff');
+    use xPaw\SourceQuery\SourceQuery;
+    Header('X-Content-Type-Options: nosniff');
 
-  $json = file_get_contents("servers.json");
-  $servers = json_decode($json,true);
-  $totalJogadores = 0;
-  $maxJogadores = 0;
-  $info = [];
+    $json = file_get_contents("servers.json");
+    if($json === FALSE) { ?>
+      <div class="card-panel red lighten center-align">Couldn't load the file with the server informations.</div>
+    <?php
+      exit;
+    }
 
-  define('SQ_TIMEOUT', 1);
-	define('SQ_ENGINE', SourceQuery::SOURCE);
+    $servers = json_decode($json,true);
+    $totalJogadores = 0;
+    $maxJogadores = 0;
+    $info = [];
 
-  foreach ($servers as $server) {
-    $Query = new SourceQuery();
-    try
-  	{
-  		$Query->Connect($server[0], $server[1], SQ_TIMEOUT, SQ_ENGINE);
+    define('SQ_TIMEOUT', 1);
+    define('SQ_ENGINE', SourceQuery::SOURCE);
 
-  		array_push($info, $Query->GetInfo());
-  	}
-  	catch( Exception $e )
-  	{
-  		$falha = $falha . `$server[2], `;
-  	}
-  	finally
-  	{
-  		$Query->Disconnect();
-  	}
-  }
-?>
-			<?php if($falha){ ?>
-				<div class="card-panel orange lighten center-align">Existem problemas de ligação com os seguintes servidores<b><?php echo $falha; ?></b></div>
-			<?php } ?>
-			<div class="col s12">
-				<table class="grey lighten-5 striped">
-				<?php
-        foreach($info as $pos => $server){
+    foreach ($servers as $server) {
+      $Query = new SourceQuery();
+      try
+      {
+        $Query->Connect($server[0], $server[1], SQ_TIMEOUT, SQ_ENGINE);
+        array_push($info, $Query->GetInfo());
+      }
+      catch( Exception $e )
+      {
+        $falha = $falha . `$server[2], `;
+      }
+      finally
+      {
+        $Query->Disconnect();
+      }
+    }
+    ?>
+    <?php if($falha){ ?>
+    <div class="card-panel orange lighten center-align">Existem problemas de ligação com os seguintes servidores<b><?php echo $falha; ?></b></div>
+    <?php } ?>
+    <div class="col s12">
+      <table class="grey lighten-5 striped">
+        <?php
+          foreach($info as $pos => $server){
           $totalJogadores = $totalJogadores + $server['Players'];
           $maxJogadores = $maxJogadores + $server['MaxPlayers'];
         ?>
-          <tr>
-            <td><b><?=$server['HostName'] ?></b></td><td><?=$server['Map'] ?></td><td><b><a href="steam://<?=$servers[$pos+1][0]?>:<?=$server['GamePort']?>" class="waves-effect waves-light"><?=$servers[$pos+1][0]?>:<?=$server['GamePort']?></a></b></td><td><b><?=$server['Players']?>/<?=$server['MaxPlayers']?></b></td>
-          </tr>
+        <tr>
+          <td><b><?=$server['HostName'] ?></b></td>
+          <td><?=$server['Map'] ?></td>
+          <td><b><a href="steam://<?=$servers[$pos+1][0]?>:<?=$server['GamePort']?>" class="waves-effect waves-light"><?=$servers[$pos+1][0]?>:<?=$server['GamePort']?></a></b></td>
+          <td><b><?=$server['Players']?>/<?=$server['MaxPlayers']?></b></td>
+        </tr>
         <?php } ?>
-				</table>
-			<div class="col s6">
-				<?php
-					$myFile = fopen("recorde.txt", "r+") or die("Impossível abrir o ficheiro!");
-					$recorde = fgets($myFile);
-					if($recorde < $totalJogadores){
-						file_put_contents("recorde.txt", $totalJogadores);
-						$recorde = $totalJogadores;
-					}
-					fclose($myFile);
-				?>
+      </table>
+      <div class="col s6">
+        <?php
+        $myFile = fopen("recorde.txt", "r+");
+        if($myFile == NULL) //if file does not exist, create it
+        {
+            $myFile = fopen("recorde.txt", "w+");
+            file_put_contents("recorde.txt", 0);
+            $recorde = 0;
+            if($recorde < $totalJogadores){
+              file_put_contents("recorde.txt", $totalJogadores);
+              $recorde = $totalJogadores;
+            }
+        }else{
+          $recorde = fgets($myFile);
+          if($recorde < $totalJogadores){
+            file_put_contents("recorde.txt", $totalJogadores);
+            $recorde = $totalJogadores;
+          }
+        }
+        fclose($myFile);
+        ?>
 
-				<div class="card-panel hoverable grey lighten-5">
-					<span class="title"><b>Jogadores online</b></span>
-					<a href="#!" class="secondary-content"><?=$totalJogadores ?>/<?=$maxJogadores ?></a>
-				</div>
-			</div>
-			<div class="col s6">
-				<div class="card-panel hoverable grey lighten-5">
-					<span class="title"><b>Recorde de jogadores</b></span>
-					<a href="#!" class="secondary-content"><?php echo $recorde; ?></a>
-				</div>
-				<div class="white-text right-align">made by <b><a href="http://www.drakz.pt/">João Rodrigues</a></b> (dяαкz ;3) &copy; 2018</div>
-			</div>
-		</div>
-	</div>
+        <div class="card-panel hoverable grey lighten-5">
+          <span class="title"><b>Jogadores online</b></span>
+          <a href="#!" class="secondary-content"><?=$totalJogadores ?>/<?=$maxJogadores ?></a>
+        </div>
+        <div class="white-text left-align">Available on <a href="https://www.github.com/DarkDracoon/sourceserverstatus">GitHub</a></div>
+      </div>
+      <div class="col s6">
+        <div class="card-panel hoverable grey lighten-5">
+          <span class="title"><b>Recorde de jogadores</b></span>
+          <a href="#!" class="secondary-content"><?php echo $recorde; ?></a>
+        </div>
+        <div class="white-text right-align">made by <b><a href="http://www.drakz.pt/">João Rodrigues</a></b> (dяαкz ;3) &copy; 2018</div>
+      </div>
+    </div>
+  </div>
 
   <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
